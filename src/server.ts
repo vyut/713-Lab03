ed0a107e45d2f4a91fb0d22a78d089a1db62d3eb
Weerayuth Uarjaipra <weerayuth_u@cmu.ac.th>
2025-02-19 21:33:59 +0700

    Task 9 - การสร้าง endpoint เพื่อ upload file สู่ supabase - Books

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import express, { Request, Response } from "express";
 import { getAllBooks, getBooksByGroup, getBookById, addBook } from "./services/BookService";
 
 import type { Book } from "./models/Book";
+import multer from "multer";
+import { uploadFile } from "./services/UploadFileService";
+
+const upload = multer({ storage: multer.memoryStorage() });
+
 import add from "./function";
 const app = express();
 app.use(express.json());
@@ -31,10 +36,22 @@ app.get("/books/:id", async (req, res) => {
   }
 });
 
-app.post("/books", async (req, res) => {
-  const newBook: Book = req.body;
-  await addBook(newBook);
-  res.json(newBook);
+app.post("/upload", upload.single("file"), async (req: any, res: any) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).send("No file uploaded.");
+    }
+
+    const bucket = "bucket-se713";
+    const filePath = `uploads/${file.originalname}`;
+    
+    await uploadFile(bucket, filePath, file);
+
+    res.status(200).send("File uploaded successfully.");
+  } catch (error) {
+    res.status(500).send("Error uploading file.");
+  }
 })
 
 app.listen(port, () => {
